72a6cea4899beb0116ff5b449374b5ff37f8e214
Sina <sadeghesfahani.sina@gmail.com>
2023-01-09 10:44:57 +0330

    feat(examples): atomic transaction implemented

---
+++ codes/3-atomic-transaction.py
@@ -0,0 +1,40 @@
+import logging
+import redis
+
+logging.basicConfig()
+
+
+class OutOfStockError(Exception):
+    """Raised when PyHats.com is all out of today's hottest hat"""
+
+
+def buy_item(r: redis.Redis, itemid: str) -> None:
+    with r.pipeline() as pipe:
+        error_count = 0
+        while True:
+            try:
+                # Get available inventory, watching for changes
+                # related to this itemid before the transaction
+                pipe.watch(itemid)
+                number_left: bytes = r.hget(itemid, "quantity")
+                if number_left > b"0":
+                    pipe.multi()
+                    pipe.hincrby(itemid, "quantity", -1)
+                    pipe.hincrby(itemid, "npurchased", 1)
+                    pipe.execute()
+                    break
+                else:
+                    # Stop watching the itemid and raise to break out
+                    pipe.unwatch()
+                    raise OutOfStockError(
+                        f"Sorry, {itemid} is out of stock!"
+                    )
+            except redis.WatchError:
+                # Log total num. of errors by this user to buy this item,
+                # then try the same process again of WATCH/HGET/MULTI/EXEC
+                error_count += 1
+                logging.warning(
+                    "WatchError #%d: %s; retrying",
+                    error_count, itemid
+                )
+    return None
